a34a9c130d2971fe68408c4ed66ee0a8d902ca16
jhonuru <47787645+jhonuru@users.noreply.github.com>
2024-09-06 10:04:26 -0500

    PDF || material.zip

--- src/config/global.js
+++ src/config/global.js
@@ -116,7 +116,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/63520028_CF03_DU.pdf',
       },
       {
         icono: 'fas fa-download',
